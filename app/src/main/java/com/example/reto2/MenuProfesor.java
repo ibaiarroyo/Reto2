7a730488c06493527e8cff366207d8eb88b3611b
ibai99 <ibai.arroyodi@elorrieta-errekamari.com>
2023-01-17 17:02:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/reto2/MenuProfesor.java
@@ -0,0 +1,14 @@
+package com.example.reto2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuProfesor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_profesor);
+    }
+}
